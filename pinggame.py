2bb02ff20d063cbd9d0d86fb6a80269c1fd00b10
Artemkaaaaiqoo <ak2199682@gmail.com>
2025-05-11 13:41:27 +0300

    Update pinggame.py

--- pinggame.py
+++ pinggame.py
@@ -37,21 +37,37 @@ fps = 120
 player_r = Player('rocket.png', 620, 200, 5, 50, 200)
 player_l = Player('rocket.png', 30, 200, 5, 50, 200)
 
+ball = GameSprite('ball.png', 200, 200, 5 ,50, 50)
+
+speed_x = ball.speed
+speed_y = ball.speed
+
+
 while game:
     for e in event.get(): # для всех событий системы
         if e.type == QUIT: # если событие типа нажатие на крестик
                 game = False # завершаем
     if finish != True:
         window.fill(BACK)
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        player_r.update_r()
+        player_l.update_l()
+        
+        if ball.rect.y > 500 - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player_l, ball) or sprite.collide_rect(player_r, ball):
+            speed_x *= -1
+
+
+
 
 
     player_r.reset()
     player_l.reset()
+    ball.reset()
 
-    player_r.update_r()
-    player_l.update_l()
-
-
+    
 
 
 
